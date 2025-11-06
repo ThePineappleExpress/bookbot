6fdffada309a8073b02c6e5b340ccfb8687f1473
ThePineappleExpress <mateusz.paszkowski@gmail.com>
2025-11-06 19:47:58 +0100

    E: bugfix

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def get_book_text(file_path):
 
 def split_to_words(file_content):
     num_words = 0
-    book_list = file_content.split(" ") 
+    book_list = file_content.split() 
     for word in book_list:
         num_words += 1 
     print(f"Found {num_words} total words")
