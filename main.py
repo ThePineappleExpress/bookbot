78b0e10085fd2324bcd2b6de81b917600630ea25
ThePineappleExpress <mateusz.paszkowski@gmail.com>
2025-11-06 21:58:03 +0100

    G: Added character count

--- main.py
+++ main.py
@@ -1,16 +1,32 @@
+
 def get_book_text(file_path):
     with open(file_path, encoding="utf8") as f:
         file_content = f.read()
-    return file_content       
+    return file_content
 
 def main():
-    from stats import split_to_words
+    from stats import split_to_words, pretty_counter
+    
     file_path = './books/frankenstein.txt'
     file_content = get_book_text(file_path)
-    result = split_to_words(file_content)
-    return file_content, result
-
+    list_of_letters = pretty_counter(file_content)
+    
+    print(
+    f"============ BOOKBOT ============\n"
+    f"Analyzing book found at {file_path}...\n"
+    f"----------- Word Count ----------\n"
+    f"Found {split_to_words(file_content)} total words\n"
+    f"--------- Character Count -------"
+    ) 
+    for item in list_of_letters:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+    return
+    
 
 main()
+
+
 
     
--- stats.py
+++ stats.py
@@ -1,6 +1,54 @@
 def split_to_words(file_content):
+
     num_words = 0
-    book_list = file_content.split() 
-    for word in book_list:
+    word_list = file_content.split() 
+
+    for word in word_list:
+
         num_words += 1 
-    print(f"Found {num_words} total words")
+
+    return num_words 
+
+
+
+def count_character_population(file_content):
+    file_content = file_content.lower()
+    character_list = []
+    counter = {}
+    
+    for character in file_content:
+        
+        current = counter.get(character, 0)
+        
+        if character.isalpha():
+            
+            if character not in character_list:
+            
+                character_list.append(character)
+                counter[character] = current + 1
+            
+            else:
+                counter[character] = current + 1
+
+        else:
+            continue
+
+    return counter
+
+def sorter(count):
+    return count["num"]
+
+def pretty_counter(file_content):
+    counter = count_character_population(file_content)
+    list_of_letters = []
+    for character, count in counter.items():
+        list_of_letters.append({"char": character, "num": count})
+    list_of_letters.sort(reverse=True, key=sorter)
+    
+
+    return list_of_letters
+    
+     
+
+
+
