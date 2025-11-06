0eb8f50b32a0bdabbc471d09c6147e94d43648e3
ThePineappleExpress <mateusz.paszkowski@gmail.com>
2025-11-06 19:28:21 +0100

    A: First draft

---
+++ main.py
@@ -0,0 +1,15 @@
+def get_book_text(file_path):
+    with open(file_path, encoding="utf8") as f:
+        file_content = f.read()
+    return file_content
+
+def main():
+    file_path = './books/frankenstein.txt'
+    file_content = get_book_text(file_path)
+    
+    return file_content
+
+
+main()
+
+    
