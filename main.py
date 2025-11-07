402548a1a303b636e7bed25886b63b01c85989e2
ThePineappleExpress <mateusz.paszkowski@gmail.com>
2025-11-07 01:06:48 +0100

    H: 1.0

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 def get_book_text(file_path):
     with open(file_path, encoding="utf8") as f:
@@ -7,7 +12,7 @@ def get_book_text(file_path):
 def main():
     from stats import split_to_words, pretty_counter
     
-    file_path = './books/frankenstein.txt'
+    file_path = sys.argv[1]
     file_content = get_book_text(file_path)
     list_of_letters = pretty_counter(file_content)
     
@@ -20,7 +25,7 @@ def main():
     ) 
     for item in list_of_letters:
         if item["char"].isalpha():
-            print(f"{item["char"]}: {item["num"]}")
+            print(f'{item["char"]}: {item["num"]}')
     print("============= END ===============")
     return
     
